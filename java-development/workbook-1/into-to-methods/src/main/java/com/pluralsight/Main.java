b6bb5757a3c3e59ef9778eec5654785e97e432c1
singhun1t <manvirp123@gmail.com>
2024-04-12 11:51:21 -0400

    added a intro to methods file

---
+++ java-development/workbook-1/into-to-methods/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        displayName();
+        displayFruits();
+
+    }
+    public static void displayName(){
+        System.out.println("Manvir");
+        System.out.println("Singh");
+    }
+
+    public static void displayFruits(){
+        System.out.println("Orange");
+        System.out.println("Strawberry");
+    }
+}
